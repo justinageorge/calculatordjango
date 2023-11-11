59b9f939947d3811296f6ccd577cd7116345e455
justinageorge <justinageorge742@gmail.com>
2023-11-11 22:11:02 +0530

    djangoworks

---
+++ django works/bookapplication/book/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Books(models.Model):
+    name=models.CharField(max_length=200,unique=True)
+    author=models.CharField(max_length=200)
+    price=models.PositiveIntegerField()
+    publisher=models.CharField(max_length=200)
---
+++ django works/mobileapplication/mobile/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Mobiles(models.Model):
+    name=models.CharField(max_length=200,unique=True)
+    price=models.PositiveIntegerField()
+    brand=models.CharField(max_length=200)
+    specs=models.CharField(max_length=200)
+    display=models.CharField(max_length=200)
